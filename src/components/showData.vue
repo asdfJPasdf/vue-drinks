749d4bfa9229eb7968f811eb7cdb40ec327921d3
jonas <jonas.pfammatter@lernende.bfo-vs.ch>
2022-04-13 10:43:19 +0200

    Projekt auf Github hochladen

---
+++ src/components/showData.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src= {{image}} alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">{{name}}</h5>
+    <p class="card-text">{{description}}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">{{ingredients}}</li>
+    <li class="list-group-item"></li>
+    <li class="list-group-item"></li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        name:{
+            type: String,
+            require: true,
+        },
+        description:{
+            type:String,
+            require: false,
+        },
+        image:{
+            type:String,
+            require:false,
+
+        
+        },
+        ingredients:{
+            type: String,
+            require:false,
+
+        }
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div style="position:absolute; top:0px; right:0px;" >
+       <input type="text" placeholder="Suche..." class="form-control-sm" v-model="inputName" >
+        <br> 
+    </div> 
+
+  <showData
+  :description="description"
+  :image="image"
+  :name="name"
+  :ingredients="ingredients"
+  />
+</template>
+
+<script>
+// @ is an alias to /src
+import showData from '@/components/showData.vue';
+import axios from "axios";
+
+export default {
+  name: 'HomeView',
+  components: {
+    showData
+  },
+  data() {
+    return {
+      description: "",
+      image: "",
+      name:"",
+      ingredients: "",
+      inputName:"",  
+      data:[],
+    }
+  },
+  async created(inputName) {
+    try {
+      const resCategories = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputName}`);
+      this.categories = resCategories.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+methods:
+{
+
+}
+}
+</script>
